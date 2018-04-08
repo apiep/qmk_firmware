c6ee9d4c538b4aa2a2aa866c6335211ceb03330e
apiep <apiep.oedin@gmail.com>
2018-04-08 22:24:40 +0700

    Enable contra backlight

---
+++ keyboards/contra/keymaps/apiep/config.h
@@ -0,0 +1,18 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "config_common.h"
+
+// #define PREVENT_STUCK_MODIFIERS
+#define TAPPING_TERM 150
+
+// #define BACKLIGHT_PIN B5
+#define RGB_DI_PIN F7
+#define BACKLIGHT_LEVELS 5
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 5
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+
+#endif
